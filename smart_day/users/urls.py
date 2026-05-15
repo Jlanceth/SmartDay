98f102f23f7d82e36b958f39d2ee698cce9873a0
Данил Сахаутдинов <danilgul90@gmail.com>
2026-05-15 16:13:25 +0500

    add login and register templates

--- smart_day/users/urls.py
+++ smart_day/users/urls.py
@@ -4,10 +4,34 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
 
-    path('profile/', views.profile_view, name='profile'),
-    path('settings/', views.settings_view, name='settings'),
+    path(
+        'register/',
+        views.register_view,
+        name='register'
+    ),
+
+    path(
+        'login/',
+        views.login_view,
+        name='login'
+    ),
+
+    path(
+        'logout/',
+        views.logout_view,
+        name='logout'
+    ),
+
+    path(
+        'profile/',
+        views.profile_view,
+        name='profile'
+    ),
+
+    path(
+        'settings/',
+        views.settings_view,
+        name='settings'
+    ),
 ]
--- smart_day/users/views.py
+++ smart_day/users/views.py
@@ -1,22 +1,77 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def register_view(request):
-    return HttpResponse("Register page")
+
+    if request.method == 'POST':
+
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+
+            return redirect('/')
+
+    else:
+        form = UserCreationForm()
+
+    return render(
+        request,
+        'users/register.html',
+        {'form': form}
+    )
 
 
 def login_view(request):
-    return HttpResponse("Login page")
+
+    if request.method == 'POST':
+
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+
+            user = form.get_user()
+
+            login(request, user)
+
+            return redirect('/')
+
+    else:
+        form = AuthenticationForm()
+
+    return render(
+        request,
+        'users/login.html',
+        {'form': form}
+    )
 
 
 def logout_view(request):
-    return HttpResponse("Logout page")
+
+    logout(request)
+
+    return redirect('/')
 
 
+@login_required
 def profile_view(request):
-    return HttpResponse("Profile page")
+
+    return render(
+        request,
+        'users/profile.html'
+    )
 
 
+@login_required
 def settings_view(request):
-    return HttpResponse("Settings page")
+
+    return render(
+        request,
+        'users/settings.html'
+    )
